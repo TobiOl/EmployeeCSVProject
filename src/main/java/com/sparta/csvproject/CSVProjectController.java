5f86b69bd7dbbd396205fd25da7a3cf9b6ab8be0
TobiOl <1712264@brunel.ac.uk>
2021-10-09 06:11:03 +0100

    reformatting to actual csv complete, added logging too

--- src/main/java/com/sparta/csvproject/CSVProjectController.java
+++ src/main/java/com/sparta/csvproject/CSVProjectController.java
@@ -6,8 +6,13 @@ import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.HashSet;
 
+import org.apache.log4j.PropertyConfigurator;
+import org.apache.log4j.Logger;
+
 public class CSVProjectController {
-    public static void CSVRead(){
+    private static Logger logger = Logger.getLogger("CSV Project Logger");
+    public ArrayList<Employee> CSVRead(){
+        PropertyConfigurator.configure("log4j.properties");
         String line = null;
         ArrayList<Employee> employeeStore = new ArrayList<Employee>();
         java.util.Date DoB, DoJ;
@@ -28,59 +33,56 @@ public class CSVProjectController {
                 Employee employeeObj = new Employee(Integer.parseInt(values[0]), values[1], values[2],values[3].charAt(0), values[4], values[5].charAt(0)
                         ,values[6] ,sqlDoB ,sqlDoJ ,Integer.parseInt(values[9]));
                 employeeStore.add(employeeObj);
-
+                logger.info("New record stored in employeeStore arraylist");
             }
 
         } catch (IOException | ParseException io) {
             io.printStackTrace();
         }
 
-        HashSet<Employee> uniqueValues = CSVProjectController.DuplicatesSearch(employeeStore);
-        ArrayList<Employee> NullRecords = EmptyFieldSearch(employeeStore);
-        int duplicatesCount = DuplicatesCount(uniqueValues, employeeStore);
-        int uniqueCount = UniqueCount(uniqueValues);
-        int emptyFieldCount = EmptyFieldCount(NullRecords);
-        int cleanFieldCount = CleanFieldCount(NullRecords, employeeStore);
-        DatabaseDriver.DatabaseHandler(uniqueValues);
-        view.PrintResults(duplicatesCount, uniqueCount, emptyFieldCount, cleanFieldCount);
+        return employeeStore;
     }
 
-    public static ArrayList<Employee> EmptyFieldSearch(ArrayList<Employee> employeeArrayListUnedited){
+    public ArrayList<Employee> EmptyFieldSearch(ArrayList<Employee> employeeArrayListUnedited){
+        PropertyConfigurator.configure("log4j.properties");
         ArrayList<Employee> NullRecords = new ArrayList<>();
         int i = 0;
         for (Object o: employeeArrayListUnedited){
             if (employeeArrayListUnedited.get(i).CheckIfValid()==false){
                 NullRecords.add(employeeArrayListUnedited.get(i));
+                logger.warn("Record with empty field found!");
             }
             i++;
         }
         return NullRecords;
     }
 
-    public static int EmptyFieldCount(ArrayList<Employee> NullRecords){
+    public int EmptyFieldCount(ArrayList<Employee> NullRecords){
         return NullRecords.size();
     }
 
-    public static int CleanFieldCount(ArrayList<Employee> NullRecords, ArrayList<Employee> employeeArrayListUnedited){
+    public int CleanFieldCount(ArrayList<Employee> NullRecords, ArrayList<Employee> employeeArrayListUnedited){
         return employeeArrayListUnedited.size() - NullRecords.size();
     }
 
-    public static HashSet DuplicatesSearch(ArrayList<Employee> employeeArrayListUnedited){
+    public HashSet DuplicatesSearch(ArrayList<Employee> employeeArrayListUnedited){
+        PropertyConfigurator.configure("log4j.properties");
         int i = 0;
         HashSet<Employee> employeeHashSet = new HashSet<Employee>();
         for (Object o: employeeArrayListUnedited){
             employeeHashSet.add(employeeArrayListUnedited.get(i));
+            logger.info("Finding duplicates");
             i++;
         }
         return employeeHashSet;
     }
-    public static int UniqueCount(HashSet<Employee> uniqueValues){
+    public int UniqueCount(HashSet<Employee> uniqueValues){
         return uniqueValues.size();
     }
-    public static int DuplicatesCount(HashSet<Employee> uniqueValues, ArrayList<Employee> employeeArrayListUnedited){
-        int count = uniqueValues.size();
-        int i2 = employeeArrayListUnedited.size();
-        int result = i2-count;
+    public int DuplicatesCount(HashSet<Employee> uniqueValues, ArrayList<Employee> employeeArrayListUnedited){
+        int uniqueEmployeeCount = uniqueValues.size();
+        int employeeListUneditedCount = employeeArrayListUnedited.size();
+        int result = employeeListUneditedCount-uniqueEmployeeCount;
         return result;
     }
 }
